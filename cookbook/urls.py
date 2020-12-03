1e0786ecedb783dcf43591c825eed82492b95104
MarcelinaWojnarowska <sz.p.marcelina_w@wp.pl>
2020-12-03 15:05:49 +0100

    Working on the ability to add and edit recipe

--- cookbook/urls.py
+++ cookbook/urls.py
@@ -3,8 +3,10 @@ from . import views
 
 urlpatterns = [
 
+    path('add', views.add_recipe, name='add_recipe'),
+    path('details/<int:recipe_id>', views.recipe_details, name='recipe_details'),
+    path('edit/<int:recipe_id>', views.edit_recipe, name='edit_recipe'),
     path('recipes', views.get_recipes, name='get_recipes'),
-    path('details/<int:recipe_id>', views.recipe_details, name='recipe_details')
 ]
 
 app_name = 'cookbook'
--- cookbook/views.py
+++ cookbook/views.py
@@ -1,12 +1,50 @@
-from django.shortcuts import render, get_object_or_404
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, redirect
 from .models import Recipe
+from .forms import RecipeForm
 
 
+@login_required
 def get_recipes(request):
     my_recipes = Recipe.objects.filter(user=request.user).order_by('-date_added')
     return render(request, 'cookbook/recipes.html', {'my_recipes': my_recipes})
 
 
+@login_required
+def add_recipe(request):
+    form = RecipeForm()
+    error = "Bad data passed in. Try again."
+
+    if request.method == 'GET':
+        return render(request, 'cookbook/new_recipe.html', {'form': form})
+    else:
+        try:
+            form = RecipeForm(request.POST, request.FILES)
+            new_recipe = form.save(commit=False)
+            new_recipe.user = request.user
+            new_recipe.save()
+            return redirect('cookbook:recipe_details', recipe_id=new_recipe.id)
+        except ValueError:
+            return render(request, 'cookbook/new_recipe.html', {'form': form, 'error': error})
+
+
+@login_required
 def recipe_details(request, recipe_id):
     recipe = Recipe.objects.get(id=recipe_id)
     return render(request, 'cookbook/recipe.html', {'recipe': recipe})
+
+
+@login_required()
+def edit_recipe(request, recipe_id):
+    recipe = Recipe.objects.get(id=recipe_id)
+
+    if request.method == 'GET':
+        form = RecipeForm(instance=recipe)
+        return render(request, 'cookbook/edit_recipe.html', {'recipe': recipe, 'form': form})
+    else:
+        try:
+            form = RecipeForm(request.POST, request.FILES, instance=recipe)
+            form.save()
+            return redirect('cookbook:recipe_details', recipe_id=recipe.id)
+        except ValueError:
+            render(request, 'cookbook/edit_recipe.html', {'recipe': recipe, 'form': form, 'error': "Bad info :/"})
